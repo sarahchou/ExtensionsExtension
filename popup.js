375682ff2a302cb4d3bd220a4fc5db64e01a4c22
sarahchou <chou.s@husky.neu.edu>
2018-01-27 19:20:11 -0500

    Added names for checkboxes

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 var images = [
-  {src: 'OverlayImages/mustache.png', target: 'upperLipTop', scale: 1, valid: function(face) { return face.faceAttributes.facialHair.moustache < 0.5 }},
-  {src: 'OverlayImages/redheadhair.png', target: 'headTop', scale: 3.3},
-    {src: 'OverlayImages/brown.png', target: 'headTop', scale: 3.3},
+  {src: 'OverlayImages/mustache.png', name: 'mustache', target: 'upperLipTop', scale: 1, valid: function(face) { return face.faceAttributes.facialHair.moustache < 0.5 }},
+  {src: 'OverlayImages/redheadhair.png', name: 'red hair', target: 'headTop', scale: 3.3},
+    {src: 'OverlayImages/brown.png', name: 'brown hair', target: 'headTop', scale: 3.3},
 ]
 
 document.addEventListener('DOMContentLoaded', function() {
 
   images.forEach(function(image){
-    $('form').append($('<input type="checkbox">').attr('name', image.src))
-    $('form').append($('<label>').text(image.src))
+    $('form').append($('<input type="checkbox">').attr('name', image.name))
+    $('form').append($('<label>').text(image.name))
   })
   $('form').on('change', function(){
     var selected = [];
     $(this).serializeArray().forEach(function(e) {
       selected.push(e.name)
     });
+    console.log('got here')
     var selectedImages = [];
     images.forEach(function(image) {
-      if (selected.indexOf(image.src) !== -1) {
+      if (selected.indexOf(image.name) !== -1) {
+
         selectedImages.push(image);
       }
     });
